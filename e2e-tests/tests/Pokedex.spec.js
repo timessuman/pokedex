bbc56e749db1dc3f756d74f12c3007a8acfa4014
timessuman <soketessuman@gmail.com>
2025-07-26 03:21:25 +0000

    e2e tests for individual pokemon

---
+++ e2e-tests/tests/Pokedex.spec.js
@@ -0,0 +1,20 @@
+import { test, expect, describe } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('/')
+
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+			page.getByText(
+				'Pokémon and Pokémon character names are trademarks of Nintendo.'
+			)
+		).toBeVisible()
+  })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
